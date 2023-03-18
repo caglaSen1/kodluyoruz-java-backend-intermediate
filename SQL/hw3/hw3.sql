8461e2b80ad28782b9e2005106b8e3059227f689
Cagla Sen <caglasenn36@gmail.com>
2023-03-18 19:05:05 +0300

    README updated

--- SQL/hw3/hw3.sql
+++ SQL/hw3/hw3.sql
@@ -1,6 +1,6 @@
 -- country tablosunda bulunan country sütunundaki ülke isimlerinden 'A' karakteri ile başlayıp 'a' karakteri ile sonlananları sıralayınız.
 SELECT * FROM country
-WHERE country LIKE 'A%';
+WHERE country LIKE 'A%a';
 
 -- country tablosunda bulunan country sütunundaki ülke isimlerinden en az 6 karakterden oluşan ve sonu 'n' karakteri ile sonlananları sıralayınız.
 SELECT * FROM country
